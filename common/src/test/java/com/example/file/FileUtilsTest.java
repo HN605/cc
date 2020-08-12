7fdf4ea886f98743931f689763f6d04003e5bb7f
chuang <397063810@qq.com>
2020-08-13 01:20:42 +0800

    file

---
+++ common/src/test/java/com/example/file/FileUtilsTest.java
@@ -0,0 +1,14 @@
+package com.example.file;
+
+public class FileUtilsTest {
+    public static void main(String[] args) {
+        String str = "Est-ce possible que tu sois en train de penser à moi lorsque tu me manques?";
+
+        String fileName = "logs/fileTest.txt";
+
+        FileUtils.write(str, fileName, "ISO-8859-1");
+
+        System.out.println(FileUtils.read(fileName, "ISO-8859-1"));
+        System.out.println(FileUtils.read(fileName));
+    }
+}
