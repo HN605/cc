9a2a0d6902555219835c0e2fd42e01052f1a0111
chuang <397063810@qq.com>
2020-11-15 20:19:49 +0800

    mybatis

--- demo/src/test/java/com/example/demo/jdbc/FieldTest.java
+++ demo/src/test/java/com/example/demo/jdbc/FieldTest.java
@@ -1,4 +1,17 @@
 package com.example.demo.jdbc;
 
+import java.lang.reflect.Field;
+
 public class FieldTest {
+    public static void main(String[] args) throws Exception {
+
+        User user = new User();
+        user.setId(1);
+        user.setUsername("chen");
+
+        Class<?> aClass = Class.forName("com.example.demo.jdbc.User");
+        Field declaredFields = aClass.getDeclaredField("username");
+        declaredFields.setAccessible(true);
+        Object o = declaredFields.get(user);
+    }
 }
